e6a298654a426ad12c32c39ad5a8c54e72b5aa76
RaiMuhammadTayyab <66311583+RaiMuhammadTayyab@users.noreply.github.com>
2021-10-18 22:53:26 +0500

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 export const client = new ApolloClient({
   link: new HttpLink({
       
- GATSBY_API_URL: '/.netlify/functions/record4',
+       uri: '/.netlify/functions/record4',
     fetch,          
   }),
   cache: new InMemoryCache()
